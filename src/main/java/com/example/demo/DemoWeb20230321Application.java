b2a5552dba0f575a808dccc105e23f34a34c6c14
boma086 <Firekiss@999>
2023-03-26 14:26:54 +0800

    sub

---
+++ src/main/java/com/example/demo/DemoWeb20230321Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeb20230321Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWeb20230321Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo/DemoWeb20230321ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeb20230321ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
